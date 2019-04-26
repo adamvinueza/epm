9b13465f1ee71946432b9f93404067a9706a034b
Adam Vinueza <adam.vinueza@mindflash.com>
2019-04-26 11:45:54 -0600

    Figured out a non-tail-recursive way of doing this, duh

--- ex1.2.4/vector-index.scm
+++ ex1.2.4/vector-index.scm
@@ -5,5 +5,12 @@
           acc
           (vector-index-acc pred (cdr v) (+ 1 acc)))))
 
-(define (vector-index pred v)
+(define (vector-index-tail pred v)
   (vector-index-acc pred v 0))
+
+(define (vector-index pred v)
+  (if (null? v)
+    #f
+    (if (pred (car v))
+      0
+      (+ 1 (vector-index pred (cdr v))))))
